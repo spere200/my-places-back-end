7598afb302b48672b9786d623154a528d7cf3e40
Samuel Perez <sperez207@gmail.com>
2023-10-29 07:08:53 -0400

    Finished working on the places routes

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,52 +1,76 @@
-const HttpError = require('../models/http-error');
-const { v4: uuid } = require('uuid');
+const HttpError = require("../models/http-error");
+const { v4: uuid } = require("uuid");
 
-const SEED_PLACES = [
+let SEED_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     location: {
       lat: 40.7484474,
-      lng: -73.9871516
+      lng: -73.9871516,
     },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
-]
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
-const getPlaceById = (req, res) => {
+exports.getPlaceById = (req, res) => {
   const placeId = req.params.pid;
-  const place = SEED_PLACES.find(p => p.id === placeId)
+  const place = SEED_PLACES.find((p) => p.id === placeId);
 
   if (!place) {
     throw new HttpError(`Place with id "${placeId}" does not exist.`, 404);
-  }
-  else {
+  } else {
     res.json({ place });
   }
-}
+};
 
-const getPlacesByUserId = (req, res, next) => {
+exports.getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
+  const places = SEED_PLACES.filter((p) => p.creator === userId);
 
-  const place = SEED_PLACES.find(p => p.creator === userId);
-
-  if (!place) {
+  if (!places) {
     next(new HttpError(`No places found for user with id "${userId}".`, 404));
+  } else {
+    res.json({ places });
   }
-  else {
-    res.json({ place });
-  }
-}
+};
 
-const createPlace = (req, res, next) => {
+exports.createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body;
-  const newPlace = { id: uuid(), title, description, coordinates, address, creator }
+  const newPlace = {
+    id: uuid(),
+    title,
+    description,
+    coordinates,
+    address,
+    creator,
+  };
   SEED_PLACES.push(newPlace);
-  res.json(newPlace)
-}
+  res.json(newPlace);
+};
 
-exports.getPlaceById = getPlaceById;
-exports.getPlacesByUserId = getPlacesByUserId;
-exports.createPlace = createPlace;
+exports.updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+  const placeIndex = SEED_PLACES.findIndex((p) => p.id === placeId);
+
+  if (placeIndex < 0) {
+    next(new HttpError(`No places found for place with id "${placeId}".`, 404));
+  } else {
+    const updatedPlace = { ...SEED_PLACES[placeIndex], title, description };
+    SEED_PLACES[placeIndex] = updatedPlace;
+
+    res.json({
+      message: `Updated place with id ${placeId}`,
+      update: SEED_PLACES[placeIndex],
+    });
+  }
+};
+
+exports.deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  SEED_PLACES = SEED_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place." });
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -3,10 +3,10 @@ const placeController = require('../controllers/places-controller');
 
 const router = express.Router();
 
-router.get('/:pid', placeController.getPlaceById);
-
 router.get('/user/:uid', placeController.getPlacesByUserId);
-
+router.get('/:pid', placeController.getPlaceById);
 router.post('/', placeController.createPlace);
+router.patch('/:pid', placeController.updatePlace);
+router.delete('/:pid', placeController.deletePlace);
 
 module.exports = router;
